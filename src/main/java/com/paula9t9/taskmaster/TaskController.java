e9f1730b0620e0a28f8c5927c9b8616f43b5cf1c
Paula Thomas <paula9t9@gmail.com>
2019-07-03 11:37:01 -0700

    Status put works

--- src/main/java/com/paula9t9/taskmaster/TaskController.java
+++ src/main/java/com/paula9t9/taskmaster/TaskController.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
@@ -30,5 +32,25 @@ public class TaskController {
         taskRepository.save(newTask);
 
         return new RedirectView("/tasks");
+    }
+
+    @PutMapping("tasks/{id}/state")
+    public ResponseEntity putState(@PathVariable String id){
+        //get task with id
+        Task task = taskRepository.findById(id).get();
+        String currentStatus = task.getStatus();
+
+        //Advance the status depending on where it currently is
+        if(currentStatus.equals("Available")){
+            task.setStatus("Assigned");
+        }else if(currentStatus.equals("Assigned")){
+            task.setStatus("Accepted");
+        }else if(currentStatus.equals("Accepted")){
+            task.setStatus("Finished");
+        } // Don't need one for finished. If finished, we do nothing.
+
+        taskRepository.save(task);
+
+        return new ResponseEntity(task, HttpStatus.OK);
     }
 }
